15f933c5f34684c6983df71cc42b95ec0023711c
P Sudeepam <sudeepam.pandey@gmail.com>
2018-12-28 21:03:46 +0530

    now we can generate compressed data files

--- Compression/compress_data.cpp
+++ Compression/compress_data.cpp
@@ -2,20 +2,36 @@
 #include <map>
 #include <string>
 #include <fstream>
-//#include <bitset>
+#include <bitset>
 #include "node_definition.h"
 #include "compress_data.h"
 
 extern std::map <char, std::string> encoder;
 
 void compress_data (std::string msg){
-  std::fstream f;
-  std::string bit_seq = "";
-  f = std::fstream("data.binary", std::ios::out | std::ios::binary);
+  std::string bit_seq = "", group = "0", encrypted = "";
   for (int i = 0; i < msg.size(); i++)
     bit_seq = bit_seq + encoder.at(msg[i]);
-  //std::bitset<sizeof(unsigned long) * 8> bits(bit_seq);
-  //unsigned long binary = bits.to_ulong();
-  //f.write((const char*)&binary, sizeof(unsigned long));
-  //std::cout << "done" << "\n";
+  std::cout << bit_seq << '\n';
+  for (int i = 0; i < bit_seq.size(); i++) {
+    group = group + bit_seq[i];
+    if (group.size() == 8) {
+      encrypted = encrypted + str_to_char(group);
+      group = "0";
+    }
+  }
+  std::string edge_case((8 - group.size()), '0');
+  edge_case = edge_case + group;
+  encrypted = encrypted + str_to_char(edge_case);
+  std::ofstream f;
+  f.open ("compressed.txt");
+  f << group.size() << encrypted;
+  std::cout << "done" << "\n";
+  f.close();
+}
+
+char str_to_char(std::string group)
+{
+    std::bitset<8> temp(group);
+    return temp.to_ulong();
 }
--- Compression/compress_data.h
+++ Compression/compress_data.h
@@ -2,5 +2,6 @@
 #define COMPRESS_H_
 
 void compress_data (std::string);
+char str_to_char(std::string);
 
 #endif
