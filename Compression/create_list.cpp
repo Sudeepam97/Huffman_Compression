c9b1789c6153e749287e9248969ce9d9954b024c
P Sudeepam <sudeepam.pandey@gmail.com>
2018-12-29 19:51:36 +0530

    changed the algorithm used to create list

--- Compression/create_list.cpp
+++ Compression/create_list.cpp
@@ -8,22 +8,18 @@ std::tuple <node*, node*, int> create_list(std::string str) {
   node *first_node = NULL; //points to the first_node of the list.
   node *last_node = NULL;  //points to the last_node node of the list.
   int list_size = 0;
-  int freq = 0;
-  std::string str_copy = str;
-  // Loop over the entire message string.
+  int freq[256] = {0};
+
   for (int i = 0; i < str.size(); i++) {
-    // Calculate the frequency of the given character
-    for (int j = 0; j < str.size(); j++) {
-      if (str[j] != '\0' && str[j] == str[i]) {
-        freq ++;
-        str_copy[j] = '\0';
-      }
-    }
-    if (str[i] != '\0'){
+    freq[int(str[i])]++;
+  }
+
+  for (int i = 0; i < str.size(); i++) {
+    if (freq[int(str[i])] != 0) {
       // Create a node and fill all its entries.
       node *new_node = new node;
       new_node->data = str[i];
-      new_node->freq = freq;
+      new_node->freq = freq[int(str[i])];
       new_node->left = NULL;
       new_node->leftc = NULL;
       new_node->right = NULL;
@@ -40,18 +36,18 @@ std::tuple <node*, node*, int> create_list(std::string str) {
       }
       // Make sure to keep track of the number of items.
       list_size++;
+      freq[int(str[i])] = 0;
     }
-    freq = 0;
-    str = str_copy;
   }
-  //print_list();
+
+  //print_list(first_node, last_node, list_size);
   std::tuple <node*, node*, int> list;
   list = std::make_tuple(first_node, last_node, list_size);
   return list;
 }
 
 /*
-void print_list(){
+void print_list(node *first_node, node *last_node, int list_size){
   node *c = first_node; //c is used as a counter variable
   while (c != NULL){
     std::cout << c->data << "\t" << c->freq << "\n";
--- Compression/create_list.h
+++ Compression/create_list.h
@@ -2,6 +2,6 @@
 #define CREATELIST_H_
 
 std::tuple <node*, node*, int> create_list(std::string);
-void print_list();
+void print_list(node* first_node, node* last_node, int list_size);
 
 #endif
